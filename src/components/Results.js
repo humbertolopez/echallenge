b57ec28b1a8d00097b38df4fcd2705d13a1f67f8
Humberto Lopez <humberto@373.mx>
2021-06-30 14:31:09 -0500

    The results list is now a component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,10 @@
 function resultsList({queryResponse}){
     return (
         <div className="resultsListContainer">
-          <ul class="resultsList">
+          <div>
+            <h2>Let's show some results:</h2>
+          </div>
+          <ul className="resultsList">
             {queryResponse.map((item)=>
               <li className="resultsListItem" key={item.id}><a href={item.url}>{item.title}</a></li>
             )}
